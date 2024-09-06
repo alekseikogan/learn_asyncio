5a6f7475eacc1bb82b24c1ebbb93077512543775
Alexey Kogan <alekseikogan@yandex.ru>
2024-09-06 15:28:53 +0300

    4_gen done

--- molchanov/4_generator.py
+++ molchanov/4_generator.py
@@ -1,11 +1,6 @@
 from time import time
 
-def generator(s):
-    for i in s:
-        yield i
-
-
-print('aaaaaaaaaaaa')
+print('Поехали!')
 s = 0
 
 
@@ -13,7 +8,7 @@ def gen_filename():
 
     global s
     while True:
- 
+
         print(f'Сумма = {s}')
         print('Давай сгенерируем название файла')
         pattern = 'file-{}.jpeg'
@@ -23,3 +18,29 @@ def gen_filename():
         print(f"Сумма = {s}")
         print()
         print(f"В прошлый раз ты сгенерировал строку {t}")
+
+
+def gen1(s):
+    for i in s:
+        yield i
+
+
+def gen2(n):
+    for i in range(n):
+        yield i
+
+
+g1 = gen1('alex')
+g2 = gen2(4)
+
+tasks = [g1, g2]
+
+while tasks:
+    task = tasks.pop(0)
+
+    try:
+        i = next(task)
+        print(i)
+        tasks.append(task)
+    except StopIteration:
+        pass
