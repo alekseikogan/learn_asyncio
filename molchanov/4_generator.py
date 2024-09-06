e6c7cae89ef25625dd40de6c0a7665b98020eb8d
Alexey Kogan <alekseikogan@yandex.ru>
2024-09-06 15:06:50 +0300

    4 баловался с генератором

--- molchanov/4_generator.py
+++ molchanov/4_generator.py
@@ -1,15 +1,25 @@
+from time import time
+
 def generator(s):
     for i in s:
         yield i
 
 
-g = generator('alex')
-print(next(g))
+print('aaaaaaaaaaaa')
+s = 0
 
-print(next(g))
 
-print(next(g))
+def gen_filename():
 
-print(next(g))
-
-print(next(g))
+    global s
+    while True:
+ 
+        print(f'Сумма = {s}')
+        print('Давай сгенерируем название файла')
+        pattern = 'file-{}.jpeg'
+        t = int(time() * 1000)
+        yield pattern.format(str(t))
+        s += 50
+        print(f"Сумма = {s}")
+        print()
+        print(f"В прошлый раз ты сгенерировал строку {t}")
